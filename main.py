85458a6245d27e50157cb7361d155f1bedfe255c
Hassaan Qayyum <hassaan_qayyum2001@hotmail.com>
2025-06-15 19:31:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# TODO:
+# handle the case when user enters the absolute path - how to identify and prevent it from executing?
+
+import os
+# Set the working directory to the directory where this script is located
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# import sys
+# import ctypes
+# from ctypes import wintypes
+from fastapi import FastAPI
+
+from routers.files import files_router
+from routers.keycloak import keycloak_router
+
+app = FastAPI()
+
+app.include_router(files_router, prefix="/files", tags=["files"])
+app.include_router(keycloak_router, prefix="/keycloak", tags=["keycloak"])
+
+@app.get("/")
+def get_items():
+    return "root_endpoint"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8003, reload=True)
